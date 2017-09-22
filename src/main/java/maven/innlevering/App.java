571888cb6414f4755b391bdda5f6d23a5de5f9c7
Hakon Schutt <hakon@schutt.no>
2017-09-22 13:58:46 +0200

    started with the basic client functionalities

--- src/main/java/maven/innlevering/App.java
+++ src/main/java/maven/innlevering/App.java
@@ -4,10 +4,25 @@ package maven.innlevering;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Readying to connect to database....");
+
+
+        //Scanner sc = new Scanner(System.in);
+        //printInstructions();
+    }
+
+    private static boolean connectToDB(String username, String password, String host){
+
+    }
+
+    private static void printInstructions(){
+        System.out.println("The following instructions are valid in this program: ");
+        System.out.println("(1) Print instructions (This page).");
+        System.out.println("(2) Scan input file to database.");
+        System.out.println("(3) Change data in table.");
+        System.out.println("(4) Print semester plan.");
+        System.out.println("(5) Quit.");
     }
 }
---
+++ src/main/java/maven/innlevering/DBHandler.java
@@ -0,0 +1,7 @@
+package maven.innlevering;
+
+/**
+ * Created by hakonschutt on 22/09/2017.
+ */
+public class DBHandler {
+}
