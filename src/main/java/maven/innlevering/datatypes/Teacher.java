c62bac0c20a4055a7367db2a37da1eb30ca0da78
Hakon Schutt <hakon@schutt.no>
2017-09-29 12:58:03 +0200

    removed datatypes from project structure

--- src/main/java/maven/innlevering/datatypes/Teacher.java
+++
@@ -1,7 +0,0 @@
-package maven.innlevering.datatypes;
-
-/**
- * Created by hakonschutt on 26/09/2017.
- */
-public class Teacher {
-}
--- src/main/java/maven/innlevering/datatypes/roomFasilities/Whiteboard.java
+++
@@ -1,7 +0,0 @@
-package maven.innlevering.datatypes.roomFasilities;
-
-/**
- * Created by hakonschutt on 26/09/2017.
- */
-public class Whiteboard {
-}
